b1aceeaabc7ab248c6cc443ae2792457a3e56a31
SNG66 <johnygutierrez1995@gmail.com>
2024-02-04 19:12:50 -0400

    2

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import git
 
 app = Flask(__name__)
 
+print('here I am')
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
+        repo = git.Repo('/home/SNG77/techtechtech')
+        print(repo)
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
